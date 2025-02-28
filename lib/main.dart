d29b5d412d25488e5bd9ff69376627d5eeb76c2f
Jayone Lee <kenbugless@gmail.com>
2025-02-28 14:40:38 +0900

    feat:스타일 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:labhub_web/core/router/router.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+import 'package:labhub_web/util/text_util.dart';
 
 void main() {
-  setUrlStrategy(PathUrlStrategy());
+  setUrlStrategy(PathUrlStrategy()); //주소창 # 해지
   runApp(const MyApp());
 }
 
@@ -16,8 +17,11 @@ class MyApp extends StatelessWidget {
       routerConfig: router,
       title: 'Flutter Demo',
       theme: ThemeData(
+        scaffoldBackgroundColor: Colors.white,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        textTheme: TextUtil.setTextTheme(),
+        fontFamily: "pretendard",
       ),
     );
   }
